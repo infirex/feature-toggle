d19fbc5487f64df1b1130bca27a60b55b76703f3
infirex <erdemahmetcelik@hotmail.com>
2025-09-09 10:57:08 +0300

    Refactor error handling in middleware and app; remove http-errors dependency

--- backend/src/app.ts
+++ backend/src/app.ts
@@ -2,11 +2,11 @@ require('dotenv').config()
 import cookieParser from 'cookie-parser'
 import cors from 'cors'
 import express, { NextFunction, Request, Response } from 'express'
-import createError, { HttpError } from 'http-errors'
 import logger from 'morgan'
 import path from 'path'
-import indexRouter from './routes/index'
 import { errorHandler } from './middleware/errorHandler'
+import indexRouter from './routes/index'
+import { AppError } from './utils/AppError'
 
 const app = express()
 
@@ -21,7 +21,7 @@ app.use('/api', indexRouter)
 
 // catch 404 and forward to error handler
 app.use((req: Request, res: Response, next: NextFunction) => {
-  next(createError(404, 'Page not found!'))
+  next(new AppError('Not Found', 404))
 })
 
 // error handler
--- backend/src/middleware/auth.ts
+++ backend/src/middleware/auth.ts
@@ -1,22 +1,20 @@
 import { NextFunction, Request, Response } from 'express'
 import jwt from 'jsonwebtoken'
 import { getAccessTokenFromHeader } from '../helpers/auth/tokenHelpers'
+import { AppError } from '../utils/AppError'
 
 const getAccess2Route = async (req: Request, res: Response, next: NextFunction) => {
   const access_token = getAccessTokenFromHeader(req)
 
   if (!access_token) {
-    return res.status(403).json({
-      success: false,
-      message: 'You are not authorized to access this route'
-    })
+    return next(new AppError('You are not authorized to access this route', 403))
   }
 
   const { JWT_SECRET } = process.env
 
   jwt.verify(access_token, JWT_SECRET as string, (err: any, decoded: any) => {
     if (err) {
-      return next(err)
+      return next(new AppError('You are not authorized to access this route', 403))
     }
     req.user = decoded as { id: string; email: string }
 
